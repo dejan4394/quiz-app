dc78301ce611389c3bb967e5e1650372c0167147
dejan4394 <dejanmalinov@yahoo.com>
2022-01-14 15:52:36 +0100

    Demo Version

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ if( process.env.NODE_ENV === 'production' ) {
   //Set static folder
   app.use(express.static('./client/build'))
 
-  app.get( '*', ( req, res ) => {
+  app.get( '/*', ( req, res ) => {
     res.sendFile(path.resolve('./client/build/index.html'))
   } )
 }
