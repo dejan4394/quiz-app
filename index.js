d597e81af1681c60536738e3274f5fd5de842282
dejan4394 <dejanmalinov@yahoo.com>
2022-01-14 09:21:13 +0100

    Demo Version

--- index.js
+++ index.js
@@ -57,10 +57,10 @@ app.use("/quizes", DeleteQuiz)
 // If in production...================
 if( process.env.NODE_ENV === 'production' ) {
   //Set static folder
-  app.use(express.static(path.join(dirname('/client/build'))))
+  app.use(express.static(path.join(dirname('./client/build'))))
 
   app.get( '*', ( req, res ) => {
-    res.sendFile(path.join(dirname('/client/build/index.html')))
+    res.sendFile(path.join(dirname('./client/build/index.html')))
   } )
 }
 
