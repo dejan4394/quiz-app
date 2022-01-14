8617b66b1dfd6d28509aed1e45843ff3938d989e
dejan4394 <dejanmalinov@yahoo.com>
2022-01-14 10:14:20 +0100

    Demo Version

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use(cookieParser())
-app.use(express.static("public"));
+app.use(express.static("./client/public"));
 app.use(
       session({
         secret: "secretcode",
@@ -57,7 +57,7 @@ app.use("/quizes", DeleteQuiz)
 // If in production...================
 if( process.env.NODE_ENV === 'production' ) {
   //Set static folder
-  app.use(express.static(path.join(__dirname,'.','client','build')))
+  app.use(express.static('./client/build')))
 
   app.get( '*', ( req, res ) => {
     res.sendFile(path.join(__dirname,'.','client','build','index.html'))
